711952ba4da0c61cdc0a33613dda3bff62cf832b
okutamatanuki <seinoazumi@gmail.com>
2019-12-15 21:42:44 +0900

    toggle side nav

--- main.js
+++ main.js
@@ -52,4 +52,20 @@ $(document).ready(function(){
         $($this.find('a').attr('href')).show();
       });
   });
+
+  /*---------------------------------------------
+    サイドメニューを表示
+  ---------------------------------------------*/
+
+  var $rightNavBtn = $('#right-nav-btn'),
+      $spRightNav = $('aside').find('.sp-right-nav'),
+      $navCloseBtn = $('aside').find('#nav-close-btn');
+
+  $rightNavBtn.click(function(){
+    $spRightNav.addClass('open');
+  });
+
+  $navCloseBtn.click(function(){
+    $spRightNav.removeClass('open');
+  });
 });
